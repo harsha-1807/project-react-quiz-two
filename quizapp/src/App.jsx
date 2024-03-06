caf2d5b1d558f7ed2379dcbcf0d2d5bddd8784c5
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-06 12:54:58 +0530

    deployment done

---
+++ quizapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+// import Mainpage from './Components/Mainpage'
+import "./App.css"
+import Main from './components/Main'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+        <Main/>
+      </div>
+    )
+  }
+}
